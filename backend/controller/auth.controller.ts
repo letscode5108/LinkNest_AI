ca9afc0ae0df63f99cb90a9e731eced704eaaad2
Ankita Chatterjee <138643835+letscode5108@users.noreply.github.com>
2025-06-27 22:51:29 +0530

    Update auth.controller.ts

--- backend/controller/auth.controller.ts
+++ backend/controller/auth.controller.ts
@@ -7,10 +7,10 @@ import { Sign } from 'crypto';
 const prisma = new PrismaClient();
 
 
-const JWT_SECRET = process.env.JWT_ACCESS_SECRET || 'your-secret-key';
-const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'your-refresh-secret-key';
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '15m';
-const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '7d';
+const JWT_SECRET = process.env.JWT_ACCESS_SECRET ;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET ;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN ;
+const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN ;
 
 interface AuthRequest extends Request {
   user?: any;
